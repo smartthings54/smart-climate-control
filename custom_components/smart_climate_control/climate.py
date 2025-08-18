33ab5e197ad94a70f4ed92268a4b5d647b7c886c
smartthings54 <73444297+smartthings54@users.noreply.github.com>
2025-08-18 20:26:28 +1200

    Update climate.py

--- custom_components/smart_climate_control/climate.py
+++ custom_components/smart_climate_control/climate.py
@@ -136,27 +136,28 @@ class SmartClimateEntity(ClimateEntity, RestoreEntity):
                 self.coordinator.comfort_temp = temperature
             await self.coordinator.async_update()
 
-async def async_set_hvac_mode(self, hvac_mode: HVACMode) -> None:
-        """Set new target hvac mode."""
-        self._attr_hvac_mode = hvac_mode
-        
-        if hvac_mode == HVACMode.OFF:
-            self.coordinator.enabled = False
-        else:
-            self.coordinator.enabled = True
-            # Remember the last active mode (HEAT or AUTO)
-            if hvac_mode in [HVACMode.HEAT, HVACMode.AUTO]:
-                self._attr_last_active_mode = hvac_mode
+    async def async_set_hvac_mode(self, hvac_mode: HVACMode) -> None:
+            """Set new target hvac mode."""
+            self._attr_hvac_mode = hvac_mode
             
-        if hvac_mode == HVACMode.HEAT:
-            self.coordinator.override_mode = True
-        else:
-            self.coordinator.override_mode = False
-            
-        await self.coordinator.async_update()
-
-    async def async_turn_on(self) -> None:
-        """Turn the entity on."""
-        # Use the last active mode, default to HEAT
-        last_mode = getattr(self, '_attr_last_active_mode', HVACMode.HEAT)
-        await self.async_set_hvac_mode(last_mode)
+            if hvac_mode == HVACMode.OFF:
+                self.coordinator.enabled = False
+            else:
+                self.coordinator.enabled = True
+                # Remember the last active mode (HEAT or AUTO)
+                if hvac_mode in [HVACMode.HEAT, HVACMode.AUTO]:
+                    self._attr_last_active_mode = hvac_mode
+                
+            if hvac_mode == HVACMode.HEAT:
+                self.coordinator.override_mode = True
+            else:
+                self.coordinator.override_mode = False
+                
+            await self.coordinator.async_update()
+    
+        async def async_turn_on(self) -> None:
+            """Turn the entity on."""
+            # Use the last active mode, default to HEAT
+            last_mode = getattr(self, '_attr_last_active_mode', HVACMode.HEAT)
+            await self.async_set_hvac_mode(last_mode)
+    
